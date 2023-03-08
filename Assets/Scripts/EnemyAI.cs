4b503881a52ea665fcaf7a9858dfe5e7599351b9
jsyeom <jsyeom@allm.co.kr>
2023-03-08 12:02:50 +0900

    156.onDrawGizmosSelected() 함수 사용하기

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,44 +1,76 @@
 using UnityEngine;
 using UnityEngine.AI;
-using static UnityEditor.PlayerSettings;
 
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] private Transform target;
-    [SerializeField] private float speed = 0.5f;
-    [SerializeField] float chaseRange = 20f;
-    [SerializeField] bool debugMode = true;
+    [SerializeField][Range(0, 10)] private float speed = 1f;
+    [SerializeField][Range(0, 10)] private float returnSpeed = 0.5f;
+    [SerializeField][Range(0, 50)] private float chaseRange = 20f;
+    [SerializeField] private bool debugMode = true;
 
     private NavMeshAgent navMeshAgent;
-    private float distanceToTarget;
+    private float distanceToTarget = Mathf.Infinity;
+    private Vector3 startPosition;
+    private STATE_ENEMY_MOVE stateEnemyMove;
+
+    private enum STATE_ENEMY_MOVE
+    {
+        FOLLOW,
+        RETURN
+    }
 
     private void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
-        distanceToTarget = Mathf.Infinity;
+        startPosition = transform.position;
     }
 
     private void Update()
     {
         SetDestination();
+        MoveEnemy();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Color color;
+
+        switch (stateEnemyMove)
+        {
+            case STATE_ENEMY_MOVE.FOLLOW:
+                color = Color.red;
+                break;
+
+            default:
+                color = Color.yellow;
+                break;
+        }
+
+        Gizmos.color = color;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+        Debug.DrawLine(transform.position, target.position, color);
     }
 
     private void SetDestination()
     {
         distanceToTarget = Vector3.Distance(transform.position, target.position);
+        stateEnemyMove = (distanceToTarget <= chaseRange) ? STATE_ENEMY_MOVE.FOLLOW : STATE_ENEMY_MOVE.RETURN;
+    }
 
-        if (chaseRange < distanceToTarget)
+    private void MoveEnemy()
+    {
+        switch (stateEnemyMove)
         {
-            Debug.DrawLine(transform.position, target.position, Color.yellow);
-        }
-        else
-        {
-            navMeshAgent.SetDestination(target.position);
+            case STATE_ENEMY_MOVE.FOLLOW:
+                navMeshAgent.speed = speed;
+                navMeshAgent.SetDestination(target.position);
+                break;
 
-            if (debugMode)
-            {
-                Debug.DrawLine(transform.position, target.position, Color.red);
-            }
+            case STATE_ENEMY_MOVE.RETURN:
+                navMeshAgent.speed = returnSpeed;
+                navMeshAgent.SetDestination(startPosition);
+                break;
         }
     }
 }
